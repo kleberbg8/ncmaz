df21a3db3b3829af0a7f84af8689fa2128fdbaee
kleberbg8 <kleberbg8@gmail.com>
2025-02-02 19:50:16 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
-import { getWordPressProps, WordPressTemplate } from '@faustwp/core'
-import { WordPressTemplateProps } from '../types'
-import { GetStaticProps } from 'next'
-import { REVALIDATE_TIME } from '@/contains/contants'
+import { getWordPressProps, WordPressTemplate } from '@faustwp/core';
+import { WordPressTemplateProps } from '../types';
+import { GetStaticProps } from 'next';
+import { REVALIDATE_TIME } from '@/contains/contants';
 
 export default function Page(props: WordPressTemplateProps) {
-	return <WordPressTemplate {...props} />
+	return <WordPressTemplate {...props} />;
 }
 
+// Definir revalidação específica para a home: 5 minutos (300 segundos)
 export const getStaticProps: GetStaticProps = (ctx) => {
-	return getWordPressProps({ ctx, revalidate: REVALIDATE_TIME })
-}
+	return getWordPressProps({ ctx, revalidate: 60 }); // 300s = 5 minutos
+};
