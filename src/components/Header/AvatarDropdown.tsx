040f27926fe9d88d3e7a9591db07b4f99da37890
kleberbg8 <kleberbg8@gmail.com>
2025-02-03 15:12:04 -0300

    Descrição breve das alterações 1

--- src/components/Header/AvatarDropdown.tsx
+++ src/components/Header/AvatarDropdown.tsx
@@ -78,6 +78,12 @@ export default function AvatarDropdown({ className = '' }: Props) {
 		)
 	}
 
+	const renderCreatePost = () => {
+		return (
+
+		)
+	}
+
 	const renderMenuEditProfile = () => {
 		return (
 			<Link
