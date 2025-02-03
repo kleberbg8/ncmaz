19f0e5c3eacfcaabb5a9aac3b0b3353a6dcfc369
kleberbg8 <kleberbg8@gmail.com>
2025-02-02 22:30:50 -0300

    Update  index.tsx
    desativamos a regra time home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { getWordPressProps, WordPressTemplate } from '@faustwp/core';
 import { WordPressTemplateProps } from '../types';
 import { GetStaticProps } from 'next';
-import { REVALIDATE_TIME } from '@/contains/contants';
+//import { REVALIDATE_TIME } from '@/contains/contants';
 
 export default function Page(props: WordPressTemplateProps) {
 	return <WordPressTemplate {...props} />;
