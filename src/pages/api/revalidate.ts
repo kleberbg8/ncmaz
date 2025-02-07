03f9e6e7fd0d3db25b277a6ea5fa29f6aef4b90c
kleberbg8 <kleberbg8@gmail.com>
2025-02-07 10:23:50 -0300

    wp Webhook ts2

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -6,7 +6,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   try {
+    console.log('Iniciando revalidação da página inicial...');
+    
+    // Revalidar apenas a página principal e uma específica de posts
     await res.revalidate('/');
+    await res.revalidate('/posts');
+
+    console.log('Revalidação concluída.');
+    
     return res.json({ revalidated: true });
   } catch (err: any) {
     console.error('Erro ao revalidar:', err);
